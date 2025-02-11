3df98a5fdd8339115b6a538c9e844523f3f327c8
leonardoheringer <heringerleonardo560@gmail.com>
2025-02-11 16:42:53 -0300

    Criação Classes

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Tarefas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas
+{
+    public class Tarefas
+    {
+        public int Id { get; set; } 
+        public string Descricao { get; set; }  
+        public bool Concluida { get; set; }
+
+        public Tarefa(int id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+            Concluida = false;  
+        }
+
+        public void ExibirTarefa()
+        {
+            string status = Concluida ? "[X]" : "[ ]";
+            Console.WriteLine($"{status} ID: {Id} - {Descricao}");
+        }
+    }
+
+}
